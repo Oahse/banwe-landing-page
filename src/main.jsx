f998cb69f78275f21a153c3ba2d7dcf9b0e5b204
oscaroguledo <oscarchiagoziem@gmail.com>
2025-08-08 14:20:55 +0100

    updated cover page

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
 import App from './App.jsx'
-import { ChakraProvider,defaultSystem } from '@chakra-ui/react'  // Make sure to import ChakraProvider
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <ChakraProvider value={defaultSystem}>
+    <StrictMode>
       <App />
-    </ChakraProvider>
-
     </StrictMode>
-  
 )
